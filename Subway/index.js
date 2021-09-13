edd8199477ec7ac1d1da738e4b789bcbc3170428
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2021-09-13 20:36:49 +0530

    basic

--- Subway/index.js
+++ Subway/index.js
@@ -1,3 +1,13 @@
 /* document.getElementById("count_el").innerText=5 */
-let count = 0;
-console.log(count)
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
+ 
